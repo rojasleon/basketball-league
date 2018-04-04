fa7773a0e12b50ee3792a46ac5ac473dc597fe5f
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-03 20:04:11 -0500

    adding in Animated transitions

--- src/containers/players.js
+++ src/containers/players.js
@@ -5,6 +5,8 @@ import { getPlayers } from '../api'
 import { parse } from 'query-string'
 import slug from 'slug'
 
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+
 class Players extends Component {
   state = {
     players: [],
@@ -50,30 +52,34 @@ class Players extends Component {
           } = players.find((player) => slug(player.name) === match.params.playerId)
 
           return (
-            <div className="panel">
-              <img className="avatar" src={`${avatar}`} alt={`${name}'s avatar`} />
-              <h1 className="medium-header">{name}</h1>
-              <h3 className="header">#{number}</h3>
-              <div className="row">
-                <ul className="info-list" style={{ marginRight: 80 }}>
-                  <li>
-                    Team
-                    <div>
-                      <Link style={{ color: '#68809a' }} to={`/${teamId}`}>
-                        {teamId[0].toUpperCase() + teamId.slice(1)}
-                      </Link>
-                    </div>
-                  </li>
-                  <li>Position<div>{position}</div></li>
-                  <li>PPG<div>{position}</div></li>
-                </ul>
-                <ul className="info-list">
-                  <li>APG<div>{apg}</div></li>
-                  <li>SPG<div>{spg}</div></li>
-                  <li>RPG<div>{rpg}</div></li>
-                </ul>
-              </div>
-            </div>
+            <TransitionGroup className="panel">
+              <CSSTransition key={location.key} timeout={250} classNames="fade">
+                <div className="panel">
+                  <img className="avatar" src={`${avatar}`} alt={`${name}'s avatar`} />
+                  <h1 className="medium-header">{name}</h1>
+                  <h3 className="header">#{number}</h3>
+                  <div className="row">
+                    <ul className="info-list" style={{ marginRight: 80 }}>
+                      <li>
+                        Team
+                        <div>
+                          <Link style={{ color: '#68809a' }} to={`/${teamId}`}>
+                            {teamId[0].toUpperCase() + teamId.slice(1)}
+                          </Link>
+                        </div>
+                      </li>
+                      <li>Position<div>{position}</div></li>
+                      <li>PPG<div>{position}</div></li>
+                    </ul>
+                    <ul className="info-list">
+                      <li>APG<div>{apg}</div></li>
+                      <li>SPG<div>{spg}</div></li>
+                      <li>RPG<div>{rpg}</div></li>
+                    </ul>
+                  </div>
+                </div>
+              </CSSTransition>
+            </TransitionGroup>
           )
         }} />
       </div>
